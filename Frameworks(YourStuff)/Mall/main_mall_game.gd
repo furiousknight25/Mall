18a1879dc53916717f07bc1949439ef13bd98890
Jude Bondhus <48182589+furiousknight25@users.noreply.github.com>
2026-02-23 19:12:38 -0600

    init

---
+++ Frameworks(YourStuff)/Mall/main_mall_game.gd
@@ -0,0 +1,8 @@
+extends Game
+
+func _start_game(): #this function is automatically called when the scene transitions in
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
